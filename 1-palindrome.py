dfa7c86194621eb8022aad0dd8d6da4cfc92ef79
Lukasz Sroczynski <lukasz.sroczynski@allegrogroup.com>
2017-10-20 14:58:01 +0200

    palindrome v2

--- 1-palindrome.py
+++ 1-palindrome.py
@@ -1,24 +1,24 @@
-def is_palidrome_v1(word):
+def is_palindrome_v1(word):
     return word == reverse_string(word)
 
 
-def is_palidrome_v2(word):
+def is_palindrome_v2(word):
 
     length = len(word)
     left_half = word[:length // 2]
     right_half = reverse_string(word[length - (length // 2):])
     return left_half == right_half
 
-def is_palidrome_v3(word):
+def is_palindrome_v3(word):
 
-    length = len(word)
-    index = 0
+    i = 0             # start-index of the string
+    j = len(word) - 1 # last-index of the string
 
-    for i in word:
-        if i == word[index]:
-            return False
+    while i < j and ( word[i] == word[j] ):
+        i += 1
+        j -= 1
 
-    return True
+    return j <= i
 
 def reverse_string(sentence):
     result = ''
@@ -26,7 +26,7 @@ def reverse_string(sentence):
         result = ch + result
     return result
 
-print(is_palidrome_v1(""))
-print(is_palidrome_v2("adYeda"))
-print(is_palidrome_v3("ada"))
-print(len("12345") // 2)
+# print(is_palidrome_v1(""))
+# print(is_palidrome_v2("adYeda"))
+print(is_palindrome_v3(""))
+# print(len("12345") // 2)
